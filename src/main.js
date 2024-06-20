eb948e06d9b7c0fb5c597c096e8be777cd8b16eb
Justiceman81 <Justiceman814@gmail.com>
2024-06-20 20:08:29 +0300

    fix mistakes

--- src/main.js
+++ src/main.js
@@ -25,38 +25,28 @@ refs.formEl.addEventListener('submit', e => {
       message: 'Info Search input must be filled!',
     });
     return;
-  } else {
-    searchImg(value)
-      .then(data => {
-        if (data.length === 0) {
-          throw new Error('Error! Nothing to load');
-        } else {
-          createElements(data);
-        }
-      })
-      .catch(error => {
-        iziToast.error({
-          title: 'Sorry,',
-          message:
-            'there are no images matching your search query. Please try again!',
-          color: 'red',
-        });
-      });
   }
+
   showLoader();
-  const arr = searchImg(value);
-  if (arr.length !== 0) {
-    arr.then(data => createElements(data.hits));
-  }
-  arr.catch(err => {
-    iziToast.show({
-      message:
-        'Sorry, there are no images matching your search query. Please try again!',
-      position: 'center',
+
+  searchImg(value)
+    .then(data => {
+      if (data.length === 0) {
+        throw new Error('Error! Nothing to load');
+      } else {
+        createElements(data);
+      }
+    })
+    .catch(error => {
+      iziToast.error({
+        title: 'Sorry,',
+        message:
+          'there are no images matching your search query. Please try again!',
+        color: 'red',
+      });
+    })
+    .finally(() => {
+      hideLoader();
+      refs.formEl.reset();
     });
-  });
-  arr.finally(() => {
-    hideLoader();
-    refs.formEl.reset();
-  });
 });
